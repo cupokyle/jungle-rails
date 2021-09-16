65984954ecad5b240beb635bde6f668d7ec9c7ad
cupokyle <kyleforsythcomedy@gmail.com>
2021-09-16 18:58:57 +0000

    Add working sale banner to home page

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,11 @@
 class Sale < ActiveRecord::Base
 
+  def self.active
+
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+
+  end
+
   def finished?
 
     ends_on < Date.current
@@ -9,6 +15,12 @@ class Sale < ActiveRecord::Base
   def upcoming?
 
     starts_on > Date.current
+
+  end
+
+  def active?
+
+    !upcoming? && !finished?
 
   end
 
